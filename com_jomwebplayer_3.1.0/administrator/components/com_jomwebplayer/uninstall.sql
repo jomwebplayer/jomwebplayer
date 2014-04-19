31cc42dd26f98a04cceb789797b171fd51c496a5
Jomwebplayer <jomwebplayer@gmail.com>
2014-04-19 17:13:14 +0530

    Revert "Jomwebplayer Files"
    This reverts commit 81a18d9e70412b27f630c6a87299645e6412f23d.

--- com_jomwebplayer_3.1.0/administrator/components/com_jomwebplayer/uninstall.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS `#__jomwebplayer_settings`;
-DROP TABLE IF EXISTS `#__jomwebplayer_skin`;
-DROP TABLE IF EXISTS `#__jomwebplayer_videos`;
-DROP TABLE IF EXISTS `#__jomwebplayer_category`;
-DROP TABLE IF EXISTS `#__jomwebplayer_googleads`;
